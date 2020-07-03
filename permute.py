819edf0195b8ece25befbc239e821ace50921937
Lexseal Lin <zhongjiezhex12@me.com>
2020-07-02 17:41:25 -0700

    I got it working! Far from optimal

--- permute.py
+++ permute.py
@@ -10,10 +10,10 @@ from cube import Cube
 import rank
 from move_coord import stage1_move, stage2_move
 
-def calc_stage_1_corners():
+def calc_stage1_corners():
     # stage 1 can be completed in 12 moves or fewer
-    stage_1_corners = bytearray([12]*2187)
-    stage_1_corners[0] = 0 # first entry is solved so takes 0 move to get to
+    stage1_corners = bytearray([12]*2187)
+    stage1_corners[0] = 0 # first entry is solved so takes 0 move to get to
 
     # [co_ori, eg_ori, ud_edges, last_move]
     state = array('I', [0, 0, 0, 255]) # a new cube
@@ -25,7 +25,7 @@ def calc_stage_1_corners():
     start_time = time() # logs
     while queue.qsize() > 0:
         cur_state = queue.get() # pop
-        move_count = stage_1_corners[cur_state[0]] # get the move count up to this state
+        move_count = stage1_corners[cur_state[0]] # get the move count up to this state
 
         last_move = cur_state[3]
         for move in MS:
@@ -40,9 +40,9 @@ def calc_stage_1_corners():
             stage1_move(next_state, move) # compute next state
 
             next_idx = next_state[0]
-            next_count = stage_1_corners[next_idx] # get moves count
+            next_count = stage1_corners[next_idx] # get moves count
             if next_count > move_count+1:
-                stage_1_corners[next_idx] = move_count+1
+                stage1_corners[next_idx] = move_count+1
                 next_state[3] = move
                 queue.put(next_state)
                 n += 1
@@ -51,12 +51,12 @@ def calc_stage_1_corners():
             last_print = n
             print(n, str(queue.qsize()//1000)+'k', move_count, round((time()-start_time)/60, 2))
     print(n)
-    np.save("table/stage1_corners", np.array(stage_1_corners, dtype=np.uint8))
+    np.save("table/stage1_corners", np.array(stage1_corners, dtype=np.uint8))
 
-def calc_stage_1_edges():
+def calc_stage1_edges():
     # works because stage 1 can be completed in 12 moves or fewer
-    stage_1_edges = bytearray([12]*2048*495)
-    stage_1_edges[0] = 0 # first entry is solved so takes 0 move to get to
+    stage1_edges = bytearray([12]*2048*495)
+    stage1_edges[0] = 0 # first entry is solved so takes 0 move to get to
 
     # [co_ori, eg_ori, ud_edges, last_move]
     state = array('I', [0, 0, 0, 255]) # a new cube
@@ -69,7 +69,7 @@ def calc_stage_1_edges():
     while queue.qsize() > 0:
         cur_state = queue.get() # pop
         cur_idx = cur_state[1]*495+cur_state[2]
-        move_count = stage_1_edges[cur_idx] # get the move count up to this state
+        move_count = stage1_edges[cur_idx] # get the move count up to this state
 
         last_move = cur_state[3]
         for move in MS:
@@ -84,9 +84,9 @@ def calc_stage_1_edges():
             stage1_move(next_state, move) # compute next state
 
             next_idx = next_state[1]*495+next_state[2]
-            next_count = stage_1_edges[next_idx] # get moves count
+            next_count = stage1_edges[next_idx] # get moves count
             if next_count > move_count+1:
-                stage_1_edges[next_idx] = move_count+1
+                stage1_edges[next_idx] = move_count+1
                 next_state[3] = move
                 queue.put(next_state)
                 n += 1
@@ -95,12 +95,98 @@ def calc_stage_1_edges():
             last_print = n
             print(n, str(queue.qsize()//1000)+'k', move_count, round((time()-start_time)/60, 2))
     print(n)
-    np.save("table/stage1_edges", np.array(stage_1_edges, dtype=np.uint8))
+    np.save("table/stage1_edges", np.array(stage1_edges, dtype=np.uint8))
 
-def calc_stage_2_corners():
+def calc_stage2_corners():
+    # stage 2 can be completed in 18 moves or fewer
+    stage2_corners = bytearray([18]*40320)
+    stage2_corners[0] = 0 # first entry is solved so takes 0 move to get to
 
+    # [co_perm, eg_perm, ud_perm, last_move]
+    state = array('I', [0, 0, 0, 255]) # a new cube
+    queue = SimpleQueue()
+    queue.put(state)
+
+    n = 0
+    last_print = 0
+    start_time = time() # logs
+    while queue.qsize() > 0:
+        cur_state = queue.get() # pop
+        move_count = stage2_corners[cur_state[0]] # get the move count up to this state
+
+        last_move = cur_state[3]
+        for move in G1Space: # we don't use the entire move space
+            cur_face = move//3
+            last_face = last_move//3
+            if cur_face == last_face: continue
+            elif cur_face == 3 and last_face == 1: continue
+            elif cur_face == 4 and last_face == 2: continue
+            elif cur_face == 5 and last_face == 0: continue
+
+            next_state = copy(cur_state) # get a copy of cur state
+            stage2_move(next_state, move) # compute next state
+
+            next_idx = next_state[0]
+            next_count = stage2_corners[next_idx] # get moves count
+            if next_count > move_count+1:
+                stage2_corners[next_idx] = move_count+1
+                next_state[3] = move
+                queue.put(next_state)
+                n += 1
+
+        if (n-last_print > 1000):
+            last_print = n
+            print(n, str(queue.qsize()//1000)+'k', move_count, round((time()-start_time)/60, 2))
+    print(n)
+    np.save("table/stage2_corners", np.array(stage2_corners, dtype=np.uint8))
+
+def calc_stage2_egdes():
+    # stage 2 can be completed in 18 moves or fewer
+    stage2_edges = bytearray([18]*40320*24)
+    stage2_edges[0] = 0 # first entry is solved so takes 0 move to get to
+
+    # [co_perm, eg_perm, ud_perm, last_move]
+    state = array('I', [0, 0, 0, 255]) # a new cube
+    queue = SimpleQueue()
+    queue.put(state)
+
+    n = 0
+    last_print = 0
+    start_time = time() # logs
+    while queue.qsize() > 0:
+        cur_state = queue.get() # pop
+        cur_idx = cur_state[1]*24+cur_state[2]
+        move_count = stage2_edges[cur_idx] # get the move count up to this state
+
+        last_move = cur_state[3]
+        for move in G1Space: # we don't use the entire move space
+            cur_face = move//3
+            last_face = last_move//3
+            if cur_face == last_face: continue
+            elif cur_face == 3 and last_face == 1: continue
+            elif cur_face == 4 and last_face == 2: continue
+            elif cur_face == 5 and last_face == 0: continue
+
+            next_state = copy(cur_state) # get a copy of cur state
+            stage2_move(next_state, move) # compute next state
+
+            next_idx = next_state[1]*24+next_state[2]
+            next_count = stage2_edges[next_idx] # get moves count
+            if next_count > move_count+1:
+                stage2_edges[next_idx] = move_count+1
+                next_state[3] = move
+                queue.put(next_state)
+                n += 1
+
+        if (n-last_print > 1000):
+            last_print = n
+            print(n, str(queue.qsize()//1000)+'k', move_count, round((time()-start_time)/60, 2))
+    print(n)
+    np.save("table/stage2_edges", np.array(stage2_edges, dtype=np.uint8))
 
 if __name__ == "__main__":
-    calc_stage_1_corners()
-    calc_stage_1_edges()
+    #calc_stage1_corners()
+    #calc_stage1_edges()
+    #calc_stage2_corners()
+    #calc_stage2_egdes()
     pass
--- solver.py
+++ solver.py
@@ -18,22 +18,29 @@ def print_move(move_num):
 # load tables
 stage1_corners = bytearray(np.load("table/stage1_corners.npy"))
 stage1_edges = bytearray(np.load("table/stage1_edges.npy"))
+stage2_corners = bytearray(np.load("table/stage2_corners.npy"))
+stage2_edges = bytearray(np.load("table/stage2_edges.npy"))
 
 def h1(state):
     co_idx = state[0]
     eg_idx = state[1]*495+state[2]
     return max(stage1_corners[co_idx], stage1_edges[eg_idx])
 
+def h2(state):
+    co_idx = state[0]
+    eg_idx = state[1]*24+state[2]
+    return max(stage2_corners[co_idx], stage2_edges[eg_idx])
+
 def is_goal(state):
     return state[0] == 0 and state[1] == 0 and state[2] == 0
 
 # iterative deepening depth-first search
-state, shuffle_list, cube = move_coord.shuffle(10)
+state, shuffle_list, cube = move_coord.shuffle(200)
 state.append(255) # use 255 to denote the -1st move
 state.append(0) # takes 0 moves to get there
 # state = [co_ori, eg_ori, ud_edges, last_move, depth]
 
-move_list = []
+move_list1 = []
 first_phase_complete = False
 for max_depth in range(0, 13):
     state_stack = deque()
@@ -52,10 +59,8 @@ for max_depth in range(0, 13):
         if cur_depth == max_depth:
             if is_goal(cur_state):
                 print("victory", cur_depth)
-                for move in move_stack:
-                    move_list.append(move)
-                for move in move_list:
-                    print_move(move)
+                for _ in range(cur_depth):
+                    move_list1.insert(0, move_stack.pop())
                 first_phase_complete = True
                 break
         else:
@@ -81,6 +86,62 @@ for max_depth in range(0, 13):
     if first_phase_complete: break
     print("level", max_depth, "done")
 
-for move in move_list:
+for move in move_list1:
     cube.move(move)
+    print_move(move)
+print(list(cube.corners), list(cube.edges))
+
+state[0] = rank.co_perm(cube.get_co_perm())
+state[1] = rank.eg_perm(cube.get_eg_perm())
+state[2] = rank.ud_perm(cube.get_ud_perm())
+state[4] = 0 # depth is 0 to begin with
+move_list2 = []
+second_phase_complete = False
+for max_depth in range(0, 19):
+    state_stack = deque()
+    move_stack = deque()
+
+    state_stack.append(state)
+    while len(state_stack) > 0:
+        cur_state = state_stack.pop()
+        cur_depth = cur_state[4] # get the depth of the state
+        while (len(move_stack) > cur_depth):
+            move_stack.pop()
+
+        last_move = cur_state[3]
+        move_stack.append(last_move)
+
+        if cur_depth == max_depth:
+            if is_goal(cur_state):
+                print("victory", cur_depth)
+                for _ in range(cur_depth):
+                    move_list2.insert(0, move_stack.pop())
+                second_phase_complete = True
+                break
+        else:
+            move_space = bytearray(G1Space)
+            random.shuffle(move_space)
+            for move in move_space:
+                cur_face = move//3
+                last_face = last_move//3
+                if cur_face == last_face: continue
+                elif cur_face == 3 and last_face == 1: continue
+                elif cur_face == 4 and last_face == 2: continue
+                elif cur_face == 5 and last_face == 0: continue
+
+                new_state = copy(cur_state)
+                move_coord.stage2_move(new_state, move)
+                new_state[3] = move
+                next_depth = cur_depth+1
+                new_state[4] = next_depth
+
+                # add if within the search range
+                if next_depth + h2(new_state) <= max_depth:
+                    state_stack.append(new_state)
+    if second_phase_complete: break
+    print("level", max_depth, "done")
+
+for move in move_list2:
+    cube.move(move)
+    print_move(move)
 print(list(cube.corners), list(cube.edges))
