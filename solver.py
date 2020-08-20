dc3d654780ddaa8bb9c1bfa348cc10e5b3298c1f
Lexseal Lin <zhongjiezhex12@me.com>
2020-08-20 11:59:53 -0700

    added reset

--- solver.py
+++ solver.py
@@ -41,7 +41,6 @@ args = parser.parse_args()
 max_move = 23
 num_of_shuffles = 100
 num_of_solves = 1
-one_solve = False
 if args.number != None:
     num_of_solves = args.number
 
@@ -55,29 +54,25 @@ for n in range(num_of_solves):
     # iterative deepening depth-first search
     if args.str != None:
         init_state, init_cube = move_coord.cube_from_str(args.str)
-        one_solve = True
     elif args.moves != None:
         init_state, init_cube = move_coord.cube_from_scramble(args.moves)
-        one_solve = True
     elif args.camera:
         import recog_color
         cube_str = recog_color.scan()
         print(cube_str)
         init_state, init_cube = move_coord.cube_from_str(cube_str)
-        one_solve = True
     else:
         init_state, shuffle_list, init_cube = move_coord.shuffle(num_of_shuffles)
     init_state.append(255) # use 255 to denote the -1st move
     init_state.append(0) # takes 0 moves to get there
     # init_state = [co_ori, eg_ori, ud_edges, last_move, depth]
 
-    start_time = time()
-
     stage1_min = 0
     times_failed = 0
     solution_found = False
     solution = []
-
+    start_time = time()
+    last_reset = start_time
     while not solution_found:
         # TODO we are assuming the cube is not solved
         pool = multiprocessing.Pool(8)
@@ -144,7 +139,11 @@ for n in range(num_of_solves):
             solution = move_list1+move_list2
         else:
             times_failed += 1
-            if stage1_min < 9:
+            if time()-last_reset > 2: # reset basically
+                stage1_min = 9
+                times_failed = 0
+                last_reset = time()
+            elif stage1_min < 9:
                 stage1_min += 1
             elif times_failed%7 == 0:
                 stage1_min = min(stage1_min+1, 12) # can't be greater than 12
@@ -159,9 +158,5 @@ for n in range(num_of_solves):
     #    init_cube.move(move)
     #print(init_cube)
     #print(list(init_cube.corners), list(init_cube.edges))
-    
-    if one_solve:
-        break
 
-print(sum(time_list)/len(time_list))
-pool.terminate()
+print("Avg time:", sum(time_list)/len(time_list))
